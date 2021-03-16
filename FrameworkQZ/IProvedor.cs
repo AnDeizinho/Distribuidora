25e3350c00902713f590256f84d178b64c7d04ea
andrey <queirozandrey5@gmail.com>
2021-03-16 17:39:32 -0400

    vai

---
+++ FrameworkQZ/IProvedor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+namespace FrameworkQZ
+{
+    public interface IProvedor
+    {
+        IDbConnection Con { get; set; }
+        IDbCommand Cmd { get; set; }
+        IDbDataAdapter Adp { get; set; }
+        IProvedor(IEntidade Ent);
+        DataTable Select(string strSelect, string Where);
+        IEntidade SelectEnt(string Where);
+        List<IEntidade> SelectEntCollection(string Where);
+        void GerarParametros();
+        void AtualizarParametros();
+        int Insert(string campos, string valores);
+        int Update(string sets, string where);
+        int Delete(string where);
+        int ExecuteNonQuery(string strCmd);
+
+
+    }
+}
---
+++ FrameworkQZ/teste.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrameworkQZ
+{
+    class teste : IProvedor
+    {
+        public teste()
+        {
+           
+        }
+        public System.Data.IDbConnection Con
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public System.Data.IDbCommand Cmd
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public System.Data.IDbDataAdapter Adp
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public System.Data.DataTable Select(string strSelect, string Where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEntidade SelectEnt(string Where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IEntidade> SelectEntCollection(string Where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GerarParametros()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AtualizarParametros()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(string campos, string valores)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(string sets, string where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(string where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExecuteNonQuery(string strCmd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
